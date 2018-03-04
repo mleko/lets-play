42a6144de0a6c515b42c85ee3a5e9ef96567ca30
Daniel Król <daniel@krol.me>
2018-03-04 07:49:32 +0100

    Init backend

---
+++ backend/src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Mleko\LetsPlay\Controller;
+
+class IndexController {
+
+    public function index() {
+        return new \Symfony\Component\HttpFoundation\JsonResponse(
+            [
+                "module" => "lets-play"
+            ]
+        );
+    }
+
+}
