69301ec94bd8a1db19e63c902f7be418ee0a4772
Daniel Król <daniel@krol.me>
2018-06-30 09:43:46 +0200

    Add knock-out stage clarification

--- frontend/src/component/GameView/Rules.tsx
+++ frontend/src/component/GameView/Rules.tsx
@@ -1,33 +1,45 @@
 import * as React from "react";
 
 import Table, {TableBody, TableCell, TableHead, TableRow} from "material-ui/Table";
+import Typography from "material-ui/Typography";
 import {Trans} from "react-i18next";
 
 export class Rules extends React.PureComponent<{}, {}> {
 	public render(): JSX.Element {
 		return (
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell><Trans>Description</Trans></TableCell>
-						<TableCell numeric={true}><Trans>Points</Trans></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					<TableRow>
-						<TableCell><Trans>Predicting exact match score</Trans></TableCell>
-						<TableCell numeric={true}>3</TableCell>
-					</TableRow>
-					<TableRow>
-						<TableCell><Trans>Predicting correct match result</Trans></TableCell>
-						<TableCell numeric={true}>1</TableCell>
-					</TableRow>
-					<TableRow>
-						<TableCell><Trans>Missed pick</Trans></TableCell>
-						<TableCell numeric={true}>0</TableCell>
-					</TableRow>
-				</TableBody>
-			</Table>
+			<div>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell><Trans>Description</Trans></TableCell>
+							<TableCell numeric={true}><Trans>Points</Trans></TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						<TableRow>
+							<TableCell><Trans>Predicting exact match score</Trans></TableCell>
+							<TableCell numeric={true}>3</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell><Trans>Predicting correct match result</Trans></TableCell>
+							<TableCell numeric={true}>1</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell><Trans>Missed pick</Trans></TableCell>
+							<TableCell numeric={true}>0</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+				<div style={{margin: "1em", textAlign: "justify"}}>
+					<Typography variant="body2" gutterBottom={true}>
+						<Trans>Knock-out stage</Trans>
+					</Typography>
+					<Typography>
+						<Trans>Bets will be settled on the result at the end of the regular game time which includes
+							injury time but excludes official extra time and penalty shoot outs</Trans>.
+					</Typography>
+				</div>
+			</div>
 		);
 	}
 }
